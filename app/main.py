cc79eaa54e1dfcd96e981ed5aaa5918f4dbb8e88
starr4ever@TransEdenV4 <starflowerr777@gmail.com>
2024-09-24 17:03:41 +0200

    impl: basic tree structure

--- app/main.py
+++ app/main.py
@@ -1,7 +1,53 @@
-pass
+# tree-unfumbler
+# For Project Nitrox² revival undertaking, made with hope and pure unadulterated 'tism 
+#
+################
+#
+# main.py
+#
+import argparse
+from pathlib import Path
+
+
+type Node_t = Tree()
+
+
+class Tree(object):
+    def __init__(self, filename=None, filetype=None, children=None):
+        self.filename: str = filename 
+        self.filetype: str = filetype
+        self.children: list[Node_t] = list() 
+        if children is not None:
+            for child in children:
+                self.add_child(child)
+    
+
+    def add_child(self, child: Node_t):
+        assert isinstance(child, Tree)
+        self.children.append(child)
+
+
+    def to_string(self) -> str:
+        if self.filetype == "/":
+            if self.children != None:
+                tree_str: str = self.filename + "/"
+                for child in self.children:
+                    tree_str = tree_str + child.to_string()
+                return tree_str
+
+            return str(self.filename + "/")
+
+        return str(self.filename + "." + self.filetype)
+
 
 if __name__ == "__main__":
     print("let's go")
+    tree = Tree("somescript", "lua")
+    print(tree.to_string())
+    tree2 = Tree("directory", "/")
+    print(tree2.to_string())
+    tree2.add_child(tree)
+    print(tree2.to_string())
 
 ####### Copyleft LVSA 09.2024
 # Terms of use:
